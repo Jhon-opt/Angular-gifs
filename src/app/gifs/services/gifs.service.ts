3054ee3d0a97448008540678f66d638c521ba9e7
Jhon <jhon.durango18@gmail.com>
2025-05-06 23:28:26 -0500

    Logica masonry implementada

--- src/app/gifs/services/gifs.service.ts
+++ src/app/gifs/services/gifs.service.ts
@@ -7,14 +7,14 @@ import { GifsMapper } from '../mapper/gifs.mapper';
 import { map, tap } from 'rxjs';
 
 const GIFS_KEY = 'gifsHistory';
-const loadfromLocalStorage = () =>{
+const loadfromLocalStorage = () => {
   const gifsStringLocalStorage = localStorage.getItem(GIFS_KEY) ?? '{}';
   const gifsL = JSON.parse(gifsStringLocalStorage);
-  console.log({gifsL});
+
   return gifsL;
 }
 
-@Injectable({providedIn: 'root'})
+@Injectable({ providedIn: 'root' })
 
 export class GifsService {
 
@@ -22,61 +22,72 @@ export class GifsService {
 
   private http = inject(HttpClient);
   trendingGifs = signal<Gif[]>([]);
-  trendingGifsLoading =signal(true);
-//logica para dividir los gifs en grupos de 3 para el masonry
-trendingGifGroup = computed<Gif[][]>(() => {
-  const groups = [];
-  const gifs = this.trendingGifs(); // Obtenemos el array de GIFs original
-  // Iteramos sobre el array original (`gifs`), no sobre `trendingGifGroup`
-  for (let index = 0; index < gifs.length; index += 3) {
-    groups.push(gifs.slice(index, index + 3)); // Agrupamos de 3 en 3
-  }
-  console.log(groups); // Debug opcional
-  return groups;
-});
+  trendingGifsLoading = signal(false);
+   number1 =0;
+
+  private trendingPage = signal(this.number1);
+
+  //logica para dividir los gifs en grupos de 3 para el masonry
+  trendingGifGroup = computed<Gif[][]>(() => {
+    const groups = [];
+    const gifs = this.trendingGifs(); // Obtenemos el array de GIFs original
+    // Iteramos sobre el array original (`gifs`), no sobre `trendingGifGroup`
+    for (let index = 0; index < gifs.length; index += 3) {
+      groups.push(gifs.slice(index, index + 3)); // Agrupamos de 3 en 3
+    }
+    console.log(groups); // Debug opcional
+    return groups;
+  });
 
   searchHistory = signal<Record<string, Gif[]>>(loadfromLocalStorage());
-  serachHistoryKeys = computed(()=> Object.keys(this.searchHistory()));
-  constructor (){
+  serachHistoryKeys = computed(() => Object.keys(this.searchHistory()));
+  constructor() {
     this.loadtrendingGifs();
   }
 
-  saveGifsToLocalStorage= effect(()=> {
+  saveGifsToLocalStorage = effect(() => {
     const historyString = JSON.stringify(this.searchHistory());
     localStorage.setItem(GIFS_KEY, historyString);
   })
   loadtrendingGifs() {
+    this.number1=+1;
+    if (this.trendingGifsLoading()) return;
+    this.trendingGifsLoading.set(true);
+
     this.http.get<GiphyResponse>(`${environment.giphy_api_url}/gifs/trending`,
       {
-        params:{
+
+        params: {
           api_key: environment.giphy_apikey,
           limit: '20',
+          offset: this.trendingPage() * 20,
 
 
         }
       }
-     ).subscribe((response) => {
-        const gifs = GifsMapper.mapGiphyItemsToGifsArray(response.data);
-        this.trendingGifs.set(gifs);
-        this.trendingGifsLoading.set(false);
-        console.log(gifs);
+    ).subscribe((response) => {
+      const gifs = GifsMapper.mapGiphyItemsToGifsArray(response.data);
+      this.trendingPage.update((page) => page + 1);
+      this.trendingGifs.update((CurrentGifs) => [...CurrentGifs, ...gifs]);
+      this.trendingGifsLoading.set(false);
 
-     });
+
+    });
   }
 
 
   searchGifs(query: string) {
     return this.http.get<GiphyResponse>(`${environment.giphy_api_url}/gifs/search`,
       {
-        params:{
+        params: {
           api_key: environment.giphy_apikey,
           q: query,
           limit: '20',
         }
       }
-     ).pipe(
-      map(({data}) => data),
-      map((items)=> GifsMapper.mapGiphyItemsToGifsArray(items)),
+    ).pipe(
+      map(({ data }) => data),
+      map((items) => GifsMapper.mapGiphyItemsToGifsArray(items)),
 
       // todo historial
       tap((items) => {
@@ -85,16 +96,16 @@ trendingGifGroup = computed<Gif[][]>(() => {
           [query.toLocaleLowerCase()]: items
         }))
       })
-     );
+    );
 
 
 
-     /*.subscribe((response) => {
-        const gifs = GifsMapper.mapGiphyItemsToGifsArray(response.data);
+    /*.subscribe((response) => {
+       const gifs = GifsMapper.mapGiphyItemsToGifsArray(response.data);
 
-        console.log({search: gifs});
+       console.log({search: gifs});
 
-     });*/
+    });*/
   }
 
   getHistoryGifs(query: string): Gif[] {
